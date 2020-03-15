5e2384bf67d21e2b0f4aa8f039f23d1f6c73aed3
zhangningle <1160030724@qq.com>
2020-03-16 00:00:53 +0800

    优化详情页，做了下拉滚动刷新

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,18 +1,20 @@
 const db = wx.cloud.database();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    booksList: []
+    books: [],
+    page: 0,
+    more: true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.getBooksList()
+    // this.getBooksList()
+    this.getList(true)
   },
   getBooksList: function () {
     let that = this;
@@ -28,7 +30,7 @@ Page({
       .orderBy('create_time', 'desc')
       .get()
       .then(res => {
-        console.log(res);
+        // console.log(res);
         let arr = res.data;
         arr.map(e => {
           e.rate = that.start(Number(e.rate))
@@ -39,6 +41,51 @@ Page({
       }).catch(err => {
         console.log(err)
       })
+  },
+  getList(init) {
+    const that = this;
+    if (init) {
+      this.setData({
+        page:0,
+        more:true
+      })
+      // 初始化
+    }
+    wx.showNavigationBarLoading()
+    wx.showLoading({ title: '加载中' })
+    const PAGE = 6;
+    const offset = this.data.page * PAGE
+    console.log(offset)
+    let ret = db.collection('doubanbooks') .orderBy('create_time', 'desc')
+    if(offset>0){
+      ret = ret.skip(offset)
+    }
+    ret = ret.limit(PAGE).get().then(books=>{
+      console.log(books)
+      let arr = books.data;
+      arr.map(e => {
+        e.rate = that.start(Number(e.rate))
+      })
+        if (arr.length < Page && this.data.page > 0) {
+          this.setData({
+            more:false
+          })
+        }
+        // books.data.length = PAGE
+        if (init) {
+          this.setData({
+            books: arr
+          })
+          wx.stopPullDownRefresh()
+        } else {
+          this.setData({
+            books: [...this.data.books, ...arr]
+          })
+          // 下拉刷新，不能直接覆盖books 而是累加
+        }
+        wx.hideLoading()
+        wx.hideNavigationBarLoading()
+    })
   },
   start(n) {
     n = Math.round(n);
@@ -54,6 +101,18 @@ Page({
     let id = e.currentTarget.id;
     wx.navigateTo({
       url: `/pages/bookdetail/bookdetail?id=${id}`
+    })
+  },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    console.log('触底了')
+    this.setData({
+      page: this.data.page+1
+    },()=>{
+      console.log(this.data.page)
+      this.getList()
     })
   },
   /**
@@ -92,12 +151,6 @@ Page({
     wx.stopPullDownRefresh()
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
 
   /**
    * 用户点击右上角分享
